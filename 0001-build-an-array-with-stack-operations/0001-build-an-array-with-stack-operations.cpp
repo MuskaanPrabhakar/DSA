b552d1543cf01a664367284f1dffb3fa8da4aa86
Muskaan <mskn36.r@gmail.com>
2026-03-16 18:13:30 +0530

    Clarify potential use of target.back() in comment
    Added a comment to clarify the use of target.back() in the buildArray function.

--- 0001-build-an-array-with-stack-operations/0001-build-an-array-with-stack-operations.cpp
+++ 0001-build-an-array-with-stack-operations/0001-build-an-array-with-stack-operations.cpp
@@ -3,6 +3,7 @@ public:
     vector<string> buildArray(vector<int>& target, int n) {
         vector<string> arr;
         int i = 1, k = 0;
+        //we could also use target.back() instead of n which would stop action if last number was directly accessed 
         while ((i) <= n && k < target.size()) {
             arr.push_back("Push");
             if (i == target[k])
