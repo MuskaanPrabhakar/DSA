4dc48a20653dbba544826c748fdb0ae5efc2ef7b
Muskaan <mskn36.r@gmail.com>
2026-03-16 17:55:38 +0530

    Time: 2 ms (10.09%), Space: 10.9 MB (20.55%) - LeetHub

--- 0001-build-an-array-with-stack-operations/0001-build-an-array-with-stack-operations.cpp
+++ 0001-build-an-array-with-stack-operations/0001-build-an-array-with-stack-operations.cpp
@@ -3,7 +3,7 @@ public:
     vector<string> buildArray(vector<int>& target, int n) {
         vector<string> arr;
         int i=1, k=0;
-            while((i)<=n && k<target.size())
+            while((i)<=target.back() && k<target.size())
             { 
                 arr.push_back("Push");
                 if(i==target[k])
