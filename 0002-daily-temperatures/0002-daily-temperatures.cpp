6ca539fed4dcfee89fe29e353751048df285622e
Muskaan <mskn36.r@gmail.com>
2026-03-21 00:16:19 +0530

    Comment out previous dailyTemperatures implementations
    Commented out previous implementations of the dailyTemperatures function for reference.

--- 0002-daily-temperatures/0002-daily-temperatures.cpp
+++ 0002-daily-temperatures/0002-daily-temperatures.cpp
@@ -16,3 +16,42 @@ public:
         return answer;
     }
 };
+/*class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<int> st;
+        vector<int> ans(temperatures.size());
+        for(int i = (temperatures.size()-1);i>=0;i--){
+            while(!st.empty() && temperatures[st.top()]<=temperatures[i]){
+                st.pop();
+            }
+            if(st.empty()){
+                ans[i] = 0;
+            }
+            else{
+                ans[i] = st.top() - i;
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};*/
+/*my first ans [inefficient]
+class Solution {//class bracket start
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {//function bracket start
+        vector<int> answer(temperatures.size());
+        for(int i=0; i<temperatures.size();i++){//for start
+            int j=i+1;
+            while(j<temperatures.size()){//while start
+                if(temperatures[i]<temperatures[j]){
+                    answer[i]=j-i;
+                    break;
+                } 
+                ++j;
+                if(j==temperatures.size()) answer[i]=0;
+            }//while close
+        }//for closed
+        return answer;
+    }
+};*/
