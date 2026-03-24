d2a1aea8e8ee52a1369466102dcb12dc7b496007
Muskaan <mskn36.r@gmail.com>
2026-03-24 23:36:32 +0530

    Improve logic for counting students unable to eat lunch
    Refactor lunch counting logic for clarity and efficiency.

--- 0001-number-of-students-unable-to-eat-lunch/0001-number-of-students-unable-to-eat-lunch.cpp
+++ 0001-number-of-students-unable-to-eat-lunch/0001-number-of-students-unable-to-eat-lunch.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
         for(int i=0; i<students.size();){
-            while(sandwiches.size()!=0 && students[0]==sandwiches[0]){
+            if(sandwiches.size()!=0 && students[0]==sandwiches[0]){
                 students.erase(students.begin());
                 sandwiches.erase(sandwiches.begin());
             }
-            students.push_back(students[0]);
-            students.erase(students.begin());
-            if(count(students.begin(), students.end(), students[0])==students.size()){
-                i=students.size();
+            else{
+                students.push_back(students[0]);
+                students.erase(students.begin());
+                if(count(students.begin(), students.end(), students[0])==students.size()){
+                     i=students.size();
+                }
             }
         }
         return students.size();
