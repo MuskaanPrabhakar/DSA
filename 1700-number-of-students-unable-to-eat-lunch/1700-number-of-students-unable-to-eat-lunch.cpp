1e4b742b0f52f27ee64a8653ee946bc6cd10e1ac
Muskaan <mskn36.r@gmail.com>
2026-03-24 23:32:59 +0530

    Time: 0 ms (100%), Space: 11.6 MB (99.02%) - LeetHub

---
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        for(int i=0; i<students.size();){
+            if(sandwiches.size()!=0 && students[0]==sandwiches[0]){
+                students.erase(students.begin());
+                sandwiches.erase(sandwiches.begin());
+            }
+            else{
+                students.push_back(students[0]);
+                students.erase(students.begin());
+                if(count(students.begin(), students.end(), students[0])==students.size()){
+                     i=students.size();
+                }
+            }
+        }
+        return students.size();
+    }
+};
