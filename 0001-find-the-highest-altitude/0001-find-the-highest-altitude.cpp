8ab54e8451935346578d0161ba76bbad6059520d
Muskaan <mskn36.r@gmail.com>
2026-04-13 18:37:43 +0530

    Time: 0 ms (100%), Space: 10.9 MB (42.01%) - LeetHub

---
+++ 0001-find-the-highest-altitude/0001-find-the-highest-altitude.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int maxi = gain[0];
+        for(int i=1; i<gain.size();i++){
+            gain[i]+=gain[i-1];
+            maxi = max(maxi,gain[i]);
+        }
+        if(maxi >=0) return maxi;
+        else return 0;
+    }
+};
