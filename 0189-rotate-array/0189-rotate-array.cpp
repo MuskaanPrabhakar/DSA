8e216bdded3ee73fd9dfae3950879a74f21f5fcb
Muskaan <mskn36.r@gmail.com>
2026-04-14 12:49:46 +0530

    Refactor rotate function using std::rotate
    Updated the rotate function implementation with a new approach using reverse

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -7,3 +7,13 @@ public:
             std::rotate(nums.begin(), nums.begin() + index, nums.begin() + index + k);
     }
 };
+/* class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+            k= k%nums.size();
+            if (k == 0) return;
+            reverse(nums.begin(),nums.end());
+            reverse(nums.begin(),nums.begin()+k);
+            reverse(nums.begin()+k,nums.end());
+    }
+}; but expected answer*/
