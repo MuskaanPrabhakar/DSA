8d604e7efe1541abaade6d58368fc3d2fd316268
Muskaan <mskn36.r@gmail.com>
2026-04-24 19:47:18 +0530

    Time: 3 ms (6.81%), Space: 9 MB (90.95%) - LeetHub

--- 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int furthestDistanceFromOrigin(string moves) {
-        int pos=0;
+        int pos=0;int mx=0;
         for(int i=0; i<moves.size(); i++){
             if(moves[i]=='L'){
                 pos--;
             }
-            else if(pos<=0 && moves[i]=='_'){
-                pos--;
+            else if(moves[i]=='_'){
+                mx++;
             }
-            else if(moves[i]=='R'|| moves[i]=='_'){
+            else if(moves[i]=='R'){
                 pos++;
             }
         }
+        if(pos<=0) pos-=mx;
+        else pos+=mx;
         pos=abs(pos);
     return pos;
     }
