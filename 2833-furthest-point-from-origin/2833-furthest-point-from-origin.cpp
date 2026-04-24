10ceacc17b5d2cfb63da0bb5d02a246809fbd19e
Muskaan <mskn36.r@gmail.com>
2026-04-24 19:52:57 +0530

    Comment out another Solution class in furthest-point-from-origin
    Commented out the another Solution class and its method.

--- 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
@@ -14,3 +14,18 @@ public:
         return abs(pos) + mx;;
     }
 };
+/*
+class Solution {
+public:
+    int furthestDistanceFromOrigin(string moves) {
+        int pos = 0, mx = 0;
+
+        for(char ch : moves) {
+            if(ch == 'L') pos--;
+            else if(ch == 'R') pos++;
+            else mx++;
+        }
+
+        return abs(pos) + mx;
+    }
+};*/
