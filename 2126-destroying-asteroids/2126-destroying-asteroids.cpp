025abd9be97e1da5b3a59f53179514c0a44e8886
Muskaan <mskn36.r@gmail.com>
2026-05-31 23:40:35 +0530

    Comment out Solution class in 2126-destroying-asteroids.cpp
    Commented out the Solution class and its method for asteroid destruction.

--- 2126-destroying-asteroids/2126-destroying-asteroids.cpp
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -30,3 +30,20 @@ public:
         return true;
     }
 };
+/*class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        sort(asteroids.begin(), asteroids.end());
+
+        long long curMass = mass;
+
+        for (int a : asteroids) {
+            if (curMass < a)
+                return false;
+
+            curMass += a;
+        }
+
+        return true;
+    }
+};*/
