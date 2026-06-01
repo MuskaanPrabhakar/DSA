4c0661ba8e6801fac299e6cc6bc33876c16a7c66
Muskaan <mskn36.r@gmail.com>
2026-06-02 00:00:06 +0530

    Time: 0 ms (100%), Space: 14.1 MB (95.51%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int minimumCost(vector<int>& cost) {
-        sort(cost.begin(), cost.end(), greater<int>());
+        sort(cost.begin(), cost.end());
 
         int mincost = 0;
 
-        for (int i = 0; i < cost.size(); i++) {
-            if ((i + 1) % 3 != 0) {
-                mincost += cost[i];
+        for (int i = cost.size() - 1; i >= 0; i -= 3) {
+            mincost += cost[i];
+            if (i - 1 >= 0) {
+                mincost += cost[i - 1];
             }
         }
 
