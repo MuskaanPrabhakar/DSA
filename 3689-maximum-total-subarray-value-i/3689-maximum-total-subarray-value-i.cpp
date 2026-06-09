eb70e05a138400e8cc3c19d6bb28c90094e217e8
Muskaan <mskn36.r@gmail.com>
2026-06-10 01:28:05 +0530

    Time: 2 ms (45.35%), Space: 103.6 MB (96.89%) - LeetHub

--- 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.cpp
+++ 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     long long maxTotalValue(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        long long maxi= nums[nums.size()-1]- nums[0];
-        
+        if (nums.size()==1) return 0LL;
+        long long mini = INT_MAX; long long maxi= 0;
+        for(int i=nums.size()-1; i>0;i--){
+            mini = min(mini, (long long)min(nums[i],nums[i-1]));
+            maxi = max(maxi, (long long)max(nums[i],nums[i-1]));
+        }
+        maxi-= mini;
         return maxi*=k;
     }
 };
