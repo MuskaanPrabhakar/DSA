fe7039e8a68090a90d3cdf6d42ccd8c2a4c789c9
Muskaan <mskn36.r@gmail.com>
2026-06-14 22:51:33 +0530

    Define ListNode structure for linked list solution
    Added definition for ListNode structure in the solution file.

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -1,4 +1,13 @@
-class Solution {
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */class Solution {
 public:
     int pairSum(ListNode* head) {
         // Find the middle of the list
