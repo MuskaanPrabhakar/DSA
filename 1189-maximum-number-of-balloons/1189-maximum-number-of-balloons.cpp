63511fe79582b34cb4750530c0d0439364092835
Muskaan <mskn36.r@gmail.com>
2026-06-22 22:50:17 +0530

    Time: 2 ms (19.9%), Space: 8.9 MB (84.03%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        int b=0; int a=0; int l=0; int o=0; int n=0;
+        for(int i=0; i<text.length(); i++){
+            if(text[i]=='b') b++;
+            else if(text[i]=='a') a++;
+            else if(text[i]=='l') l++;
+            else if(text[i]=='o') o++;
+            else if(text[i]=='n') n++;
+        }
+        l=min(l,o)/2;
+        b= min(min(b,l),min(a,n));
+        return b;
+    }
+};
