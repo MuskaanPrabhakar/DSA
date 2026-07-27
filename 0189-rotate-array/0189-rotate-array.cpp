c1d6cfb11b67e217b9c5b85da6d134f9ab87b9b3
Muskaan <mskn36.r@gmail.com>
2026-07-27 14:01:14 +0100

    Refactor rotate function implementation
    Removed unused code and comments related to array rotation.

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -3,17 +3,8 @@ public:
     void rotate(vector<int>& nums, int k) {
             k= k%nums.size();
             if (k == 0) return;
-            int index = nums.size()-k;
-            std::rotate(nums.begin(), nums.begin() + index, nums.begin() + index + k);
-    }
-};
-/* class Solution {
-public:
-    void rotate(vector<int>& nums, int k) {
-            k= k%nums.size();
-            if (k == 0) return;
             reverse(nums.begin(),nums.end());
             reverse(nums.begin(),nums.begin()+k);
             reverse(nums.begin()+k,nums.end());
     }
-}; but expected answer*/
+}
